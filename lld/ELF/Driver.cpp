ad54c3a0abe3a4665901d833d1be3806a210e784
SSE4 <tomskside@gmail.com>
2022-11-22 23:50:04 +0700

    [E2K] add an initial target for lld (as a copy of Sparc)
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- lld/ELF/Driver.cpp
+++ lld/ELF/Driver.cpp
@@ -178,6 +178,7 @@ static std::tuple<ELFKind, uint16_t, uint8_t> parseEmulation(StringRef emul) {
           .Case("elf64_sparc", {ELF64BEKind, EM_SPARCV9})
           .Case("msp430elf", {ELF32LEKind, EM_MSP430})
           .Case("elf64_amdgpu", {ELF64LEKind, EM_AMDGPU})
+          .Case("elf64e2k", {ELF64LEKind, EM_MCST_ELBRUS})
           .Default({ELFNoneKind, EM_NONE});
 
   if (ret.first == ELFNoneKind)
