f66c1c0cef30badeba24afb6ef86ffb73dc93896
SSE4 <tomskside@gmail.com>
2022-12-27 20:28:41 +0700

    [E2K] allow objdump to disassemble e2k code
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- llvm/lib/Target/E2K/Disassembler/E2KDisassembler.cpp
+++ llvm/lib/Target/E2K/Disassembler/E2KDisassembler.cpp
@@ -30,8 +30,9 @@ namespace {
 /// A disassembler class for E2K.
 class E2KDisassembler : public MCDisassembler {
 public:
+  std::unique_ptr<MCInst *> CurrentBundle;
   E2KDisassembler(const MCSubtargetInfo &STI, MCContext &Ctx)
-      : MCDisassembler(STI, Ctx) {}
+      : MCDisassembler(STI, Ctx), CurrentBundle(new MCInst *) {}
   virtual ~E2KDisassembler() = default;
 
   DecodeStatus getInstruction(MCInst &Instr, uint64_t &Size,
@@ -143,6 +144,273 @@ static const uint16_t CPPairDecoderTable[] = {
   E2K::C16_C17, E2K::C18_C19, E2K::C20_C21, E2K::C22_C23,
   E2K::C24_C25, E2K::C26_C27, E2K::C28_C29, E2K::C30_C31
 };
+
+static const unsigned DSTRegTable[] = {
+    // FIXME : check
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, E2K::TST, E2K::TC, E2K::TCD,
+    0, E2K::CTPR1, E2K::CTPR2, E2K::CTPR3,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, E2K::EMPTY_LO, E2K::EMPTY_HI,
+};
+
+enum RegSize {
+  s = 's',
+  d = 'd',
+  q = 'q',
+  p = 'p',
+};
+
+enum RegType {
+  r = 'r',
+  b = 'b',
+  g = 'g',
+};
+
+static unsigned ChooseBase(RegSize Rs, RegType Rt)
+{
+  switch (Rt) {
+  case r: {
+    switch (Rs) {
+    case s: return E2K::R0; break;
+    case d: return E2K::DR0; break;
+    case q: return E2K::QR0; break;
+    case p: return E2K::QPR0; break;
+    default: llvm_unreachable("unknown register"); break;
+    }
+  }
+  case b: {
+    switch (Rs) {
+    case s: return E2K::B0; break;
+    case d: return E2K::DB0; break;
+    case q: return E2K::QB0; break;
+    case p: return E2K::QPB0; break;
+    default: llvm_unreachable("unknown register"); break;
+    }
+  }
+  case g: {
+    switch (Rs) {
+    case s: return E2K::G0; break;
+    case d: return E2K::DG0; break;
+    case q: return E2K::QG0; break;
+    case p: return E2K::QPG0; break;
+    default: llvm_unreachable("unknown register"); break;
+    }
+  }
+  default: llvm_unreachable("unknown register"); break;
+  }
+}
+
+
+static DecodeStatus DecodeRegSTATERegisterClass(MCInst &Inst, unsigned RegNo,
+                        uint64_t Address,
+                        const MCDisassembler *Decoder) {
+  // TODO
+  Inst.addOperand(MCOperand::createReg(E2K::PSR));
+  return MCDisassembler::Success;
+}
+
+static DecodeStatus DecodeDST1RegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder,
+                                             RegSize rs) {
+
+  if (RegNo > 255)
+    return MCDisassembler::Fail;
+  if ((RegNo & 0x80) == 0) {
+    unsigned index = RegNo & 0x7F;
+    Inst.addOperand(MCOperand::createReg(ChooseBase(rs, b) + index));
+  } else if ((RegNo & 0xC0) == 0x80) {
+    unsigned index = RegNo & 0x3F;
+    Inst.addOperand(MCOperand::createReg(ChooseBase(rs, r) + index));
+  } else if ((RegNo & 0xE0) == 0xC0) {
+    unsigned index = RegNo & 0x1F;
+    unsigned reg = DSTRegTable[index];
+    if (reg == E2K::NoRegister)
+      return MCDisassembler::Fail;
+    Inst.addOperand(MCOperand::createReg(reg));
+  } else {
+    unsigned index = RegNo & 0x1F;
+    Inst.addOperand(MCOperand::createReg(ChooseBase(rs, g) + index));
+  }
+  return MCDisassembler::Success;
+}
+
+static DecodeStatus DecodeSRC1RegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder,
+                                             RegSize rs) {
+  if (RegNo > 255)
+    return MCDisassembler::Fail;
+  if ((RegNo & 0x80) == 0) {
+    unsigned index = RegNo & 0x7F;
+    Inst.addOperand(MCOperand::createReg(ChooseBase(rs, b) + index));
+  } else if ((RegNo & 0xC0) == 0x80) {
+    unsigned index = RegNo & 0x3F;
+    Inst.addOperand(MCOperand::createReg(ChooseBase(rs, r) + index));
+  } else if ((RegNo & 0xE0) == 0xC0) {
+    unsigned value = RegNo & 0x1F;
+    Inst.addOperand(MCOperand::createImm(value));
+  } else {
+    unsigned index = RegNo & 0x1F;
+    Inst.addOperand(MCOperand::createReg(ChooseBase(rs, g) + index));
+  }
+  return MCDisassembler::Success;
+}
+
+static DecodeStatus DecodeSRC2RegisterClass(MCInst &Inst, unsigned RegNo,
+                                            uint64_t Address,
+                                            const MCDisassembler *Decoder,
+                                            RegSize rs) {
+  if (RegNo > 255)
+    return MCDisassembler::Fail;
+  if ((RegNo & 0x80) == 0) {
+    unsigned index = RegNo & 0x7F;
+    Inst.addOperand(MCOperand::createReg(ChooseBase(rs, b) + index));
+  } else if ((RegNo & 0xC0) == 0x80) {
+    unsigned index = RegNo & 0x3F;
+    Inst.addOperand(MCOperand::createReg(ChooseBase(rs, r) + index));
+  } else if ((RegNo & 0xE0) == 0xC0) {
+    unsigned value = RegNo & 0x1F;
+    Inst.addOperand(MCOperand::createImm(value));
+  } else {
+    unsigned index = RegNo & 0x1F;
+    Inst.addOperand(MCOperand::createReg(ChooseBase(rs, g) + index));
+  }
+  return MCDisassembler::Success;
+}
+
+static DecodeStatus DecodeSRC3RegisterClass(MCInst &Inst, unsigned RegNo,
+                                            uint64_t Address,
+                                            const MCDisassembler *Decoder,
+                                            RegSize rs) {
+  if (RegNo > 255)
+    return MCDisassembler::Fail;
+  if ((RegNo & 0x80) == 0) {
+    unsigned index = RegNo & 0x7F;
+    Inst.addOperand(MCOperand::createReg(ChooseBase(rs, b) + index));
+  } else if ((RegNo & 0xC0) == 0x80) {
+    unsigned index = RegNo & 0x3F;
+    Inst.addOperand(MCOperand::createReg(ChooseBase(rs, r) + index));
+  } else if ((RegNo & 0xE0) == 0xC0) {
+    unsigned value = RegNo & 0x1F;
+    Inst.addOperand(MCOperand::createImm(value));
+  } else {
+    unsigned index = RegNo & 0x1F;
+    Inst.addOperand(MCOperand::createReg(ChooseBase(rs, g) + index));
+  }
+  return MCDisassembler::Success;
+}
+
+static DecodeStatus DecodeDST1SRegisterClass(MCInst &Inst, unsigned RegNo,
+                                               uint64_t Address,
+                                               const MCDisassembler *Decoder) {
+  return DecodeDST1RegisterClass(Inst, RegNo, Address, Decoder, s);
+}
+
+static DecodeStatus DecodeDST1DRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  return DecodeDST1RegisterClass(Inst, RegNo, Address, Decoder, d);
+}
+
+static DecodeStatus DecodeDST1QRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  return DecodeDST1RegisterClass(Inst, RegNo, Address, Decoder, q);
+}
+
+static DecodeStatus DecodeDST1QPRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  return DecodeDST1RegisterClass(Inst, RegNo, Address, Decoder, p);
+}
+
+
+static DecodeStatus DecodeSRC1SRegisterClass(MCInst &Inst, unsigned RegNo,
+                                          uint64_t Address,
+                                          const MCDisassembler *Decoder) {
+  return DecodeSRC1RegisterClass(Inst, RegNo, Address, Decoder, s);
+}
+
+static DecodeStatus DecodeSRC1DRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  return DecodeSRC1RegisterClass(Inst, RegNo, Address, Decoder, d);
+}
+
+static DecodeStatus DecodeSRC1QRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  return DecodeSRC1RegisterClass(Inst, RegNo, Address, Decoder, q);
+}
+
+static DecodeStatus DecodeSRC1QPRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  return DecodeSRC1RegisterClass(Inst, RegNo, Address, Decoder, p);
+}
+
+
+static DecodeStatus DecodeSRC2SRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  return DecodeSRC2RegisterClass(Inst, RegNo, Address, Decoder, s);
+}
+
+static DecodeStatus DecodeSRC2DRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  return DecodeSRC2RegisterClass(Inst, RegNo, Address, Decoder, d);
+}
+
+static DecodeStatus DecodeSRC2QRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  return DecodeSRC2RegisterClass(Inst, RegNo, Address, Decoder, q);
+}
+
+static DecodeStatus DecodeSRC2QPRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  return DecodeSRC2RegisterClass(Inst, RegNo, Address, Decoder, p);
+}
+
+static DecodeStatus DecodeSRC3SRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  return DecodeSRC3RegisterClass(Inst, RegNo, Address, Decoder, s);
+}
+
+static DecodeStatus DecodeSRC3DRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  return DecodeSRC3RegisterClass(Inst, RegNo, Address, Decoder, d);
+}
+
+static DecodeStatus DecodeSRC3QRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  return DecodeSRC3RegisterClass(Inst, RegNo, Address, Decoder, q);
+}
+
+static DecodeStatus DecodeSRC3QPRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  return DecodeSRC3RegisterClass(Inst, RegNo, Address, Decoder, p);
+}
+
+static DecodeStatus DecodePREDRegisterClass(MCInst &Inst, unsigned RegNo,
+                                             uint64_t Address,
+                                             const MCDisassembler *Decoder) {
+  if (RegNo > 31)
+    return MCDisassembler::Fail;
+  Inst.addOperand(MCOperand::createReg(E2K::PRED0 + RegNo));
+  return MCDisassembler::Success;
+}
 
 static DecodeStatus DecodeIntRegsRegisterClass(MCInst &Inst, unsigned RegNo,
                                                uint64_t Address,
@@ -320,58 +588,224 @@ static DecodeStatus DecodeTRAP(MCInst &Inst, unsigned insn, uint64_t Address,
 #include "E2KGenDisassemblerTables.inc"
 
 /// Read four bytes from the ArrayRef and return 32 bit word.
-static DecodeStatus readInstruction32(ArrayRef<uint8_t> Bytes, uint64_t Address,
-                                      uint64_t &Size, uint32_t &Insn,
-                                      bool IsLittleEndian) {
+static DecodeStatus readInstruction32(ArrayRef<uint8_t> Bytes, uint64_t & Offset,
+                                      uint64_t &Size, uint32_t &Insn) {
   // We want to read exactly 4 Bytes of data.
-  if (Bytes.size() < 4) {
+  if (Bytes.size() < Offset + 4) {
     Size = 0;
     return MCDisassembler::Fail;
   }
 
-  Insn = IsLittleEndian
-             ? (Bytes[0] << 0) | (Bytes[1] << 8) | (Bytes[2] << 16) |
-                   (Bytes[3] << 24)
-             : (Bytes[3] << 0) | (Bytes[2] << 8) | (Bytes[1] << 16) |
-                   (Bytes[0] << 24);
+  Insn = (Bytes[Offset] << 0) | (Bytes[Offset + 1] << 8) | (Bytes[Offset + 2] << 16) | (Bytes[Offset + 3] << 24);
+  Offset += 4;
 
   return MCDisassembler::Success;
 }
+
+/// Read two bytes from the ArrayRef and return 16 bit word.
+static DecodeStatus readInstruction16(ArrayRef<uint8_t> Bytes, uint64_t & Offset,
+                                      uint64_t &Size, uint16_t &Insn) {
+  // We want to read exactly 4 Bytes of data.
+  if (Bytes.size() < Offset + 2) {
+    Size = 0;
+    return MCDisassembler::Fail;
+  }
+
+  Insn = (Bytes[Offset] << 0) | (Bytes[Offset + 1] << 8);
+  Offset += 2;
+
+  return MCDisassembler::Success;
+}
+
+typedef struct als_ales_t
+{
+  uint32_t ALS;
+  uint16_t ALES;
+}
+als_ales_t;
+
+static const uint8_t * DecoderTableE2K_ALS[6] = {
+    DecoderTableE2K_ALS032,
+    DecoderTableE2K_ALS132,
+    DecoderTableE2K_ALS232,
+    DecoderTableE2K_ALS332,
+    DecoderTableE2K_ALS432,
+    DecoderTableE2K_ALS532,
+};
+
+static const uint8_t * DecoderTableE2K_ALES[6] = {
+    DecoderTableE2K_ALES048,
+    DecoderTableE2K_ALES148,
+    DecoderTableE2K_ALES248,
+    DecoderTableE2K_ALES348,
+    DecoderTableE2K_ALES448,
+    DecoderTableE2K_ALES548,
+};
+
+static const uint8_t * DecoderTableE2K_CS[2] = {
+    DecoderTableE2K_CS032,
+    DecoderTableE2K_CS132,
+};
 
 DecodeStatus E2KDisassembler::getInstruction(MCInst &Instr, uint64_t &Size,
                                                ArrayRef<uint8_t> Bytes,
                                                uint64_t Address,
                                                raw_ostream &CStream) const {
-  uint32_t Insn;
-  bool isLittleEndian = getContext().getAsmInfo()->isLittleEndian();
-  bool is64bit = getContext().getAsmInfo()->getCodePointerSize() == 8;
+  uint32_t HS; // header syllable
+  uint64_t Offset = 0;
   DecodeStatus Result =
-      readInstruction32(Bytes, Address, Size, Insn, isLittleEndian);
+      readInstruction32(Bytes, Offset, Size, HS);
   if (Result == MCDisassembler::Fail)
     return MCDisassembler::Fail;
 
-  // Calling the auto-generated decoder function.
-
-  if (is64bit)
-  {
-    Result = decodeInstruction(DecoderTableE2KV932, Instr, Insn, Address, this, STI);
+  bool HasALS[6];
+  for (uint32_t i = 0; i < 6; ++i) {
+    uint32_t MaskALS = 1 << (i + 26);
+    HasALS[i] = (HS & MaskALS) == MaskALS;
+    LLVM_DEBUG(dbgs() << "HasALS" << i << " : " << HasALS[i] << "\n");
   }
-  else
-  {
-    Result = decodeInstruction(DecoderTableE2KV832, Instr, Insn, Address, this, STI);
+  bool HasALES[6];
+  for (uint32_t i = 0; i < 6; ++i) {
+    uint32_t MaskALES = 1 << (i + 20);
+    HasALES[i] = (HS & MaskALES) == MaskALES;
+    LLVM_DEBUG(dbgs() << "HasALES" << i << " : " << HasALES[i] << "\n");
   }
-  if (Result != MCDisassembler::Fail)
-    return Result;
+  uint32_t NumPLS = (HS >> 18) & 0b11;
 
-  Result =
-      decodeInstruction(DecoderTableE2K32, Instr, Insn, Address, this, STI);
+  LLVM_DEBUG(dbgs() << "NumPLS" << " : " << NumPLS << "\n");
 
-  if (Result != MCDisassembler::Fail) {
-    Size = 4;
-    return Result;
+  uint32_t NumCDS = (HS >> 16) & 0b11;
+
+  LLVM_DEBUG(dbgs() << "NumCDS" << " : " << NumCDS << "\n");
+
+  bool HasCS[2];
+  for (uint32_t i = 0; i < 2; ++i) {
+    uint32_t MaskCS = 1 << (i + 14);
+    HasCS[i] = (HS & MaskCS) == MaskCS;
+
+    LLVM_DEBUG(dbgs() << "HasCS" << i <<  " : " << HasCS[i] << "\n");
+  }
+  const uint32_t MaskSetMark = 1 << 13;
+  bool SetMark = (HS & MaskSetMark) == MaskSetMark;
+  LLVM_DEBUG(dbgs() << "SetMark" << " : " << SetMark << "\n");
+
+  const uint32_t MaskSS = 1 << 12;
+  bool HasSS = (HS & MaskSS) == MaskSS;
+  LLVM_DEBUG(dbgs() << "HasSS" << " : " << HasSS << "\n");
+
+  const uint32_t MaskUnused = 1 << 11;
+  bool HasUnused = (HS & MaskUnused) == MaskUnused;
+  LLVM_DEBUG(dbgs() << "Unused" << " : " << HasUnused << "\n");
+
+  const uint32_t MaskLoopMode = 1 << 10;
+  bool LoopMode = (HS & MaskLoopMode) == MaskLoopMode;
+  LLVM_DEBUG(dbgs() << "LoopMode" << " : " << LoopMode << "\n");
+
+  uint32_t NOP = (HS >> 7) & 0b111;
+  LLVM_DEBUG(dbgs() << "NOP" << " : " << NOP << "\n");
+  uint32_t Length = (HS >> 4) & 0b111;
+
+  LLVM_DEBUG(dbgs() << "Length" << " : " << Length << "\n");
+
+  uint32_t Words = HS & 0b1111;
+  LLVM_DEBUG(dbgs() << "Words" << " : " << Words << "\n");
+
+  Size = (Length + 1) * 8;
+
+  als_ales_t als_ales[6];
+  uint32_t CS[2];
+
+  LLVM_DEBUG(dbgs() << "HS" << " : " << format("0x%04x", HS) << "\n");
+
+  for (uint32_t i = 0; i < 6; ++i) {
+    if (HasALS[i]) {
+      Result = readInstruction32(Bytes, Offset, Size, als_ales[i].ALS);
+      if (Result == MCDisassembler::Fail)
+        return MCDisassembler::Fail;
+      LLVM_DEBUG(dbgs() << "ALS" << i << " : " << format("0x%04x", als_ales[i].ALS) << "\n");
+    }
+  }
+  if (HasCS[0]) {
+    Result = readInstruction32(Bytes, Offset, Size, CS[0]);
+    if (Result == MCDisassembler::Fail)
+      return MCDisassembler::Fail;
+    LLVM_DEBUG(dbgs() << "CS0" << " : " << format("0x%04x", CS[0]) << "\n");
+  }
+  if (HasALES[2] != HasALES[5]) {
+    Offset += 2;
+  }
+  for (uint32_t i : {2, 5}) {
+    if (HasALES[i]) {
+      Result = readInstruction16(Bytes, Offset, Size, als_ales[i].ALES);
+      if (Result == MCDisassembler::Fail)
+        return MCDisassembler::Fail;
+      LLVM_DEBUG(dbgs() << "ALES" << i << " : " << format("0x%02x", als_ales[i].ALES) << "\n");
+    }
+  }
+  if (HasCS[1]) {
+    Result = readInstruction32(Bytes, Offset, Size, CS[1]);
+    if (Result == MCDisassembler::Fail)
+      return MCDisassembler::Fail;
+    LLVM_DEBUG(dbgs() << "CS1" << " : " << format("0x%04x", CS[1]) << "\n");
+  }
+  Offset = (Words + 1) * 4; // middle pointer
+
+  uint32_t NumALES = 0;
+  for (uint32_t i : {0, 1, 3, 4})
+    if (HasALES[i])
+        ++NumALES;
+  if (NumALES % 2 == 1)
+    Offset += 2;
+
+  for (uint32_t i : {0, 1, 3, 4}) {
+    if (HasALES[i]) {
+      Result = readInstruction16(Bytes, Offset, Size, als_ales[i].ALES);
+      if (Result == MCDisassembler::Fail)
+        return MCDisassembler::Fail;
+      LLVM_DEBUG(dbgs() << "ALES" << i << " : " << format("0x%02x", als_ales[i].ALES) << "\n");
+    }
   }
 
-  return MCDisassembler::Fail;
+  *CurrentBundle = &Instr;
+  Instr.setOpcode(E2K::BUNDLE);
+
+  if (NOP > 0) {
+    MCInst *SubInstr = getContext().createMCInst();
+    SubInstr->setOpcode(E2K::NOOP);
+    SubInstr->addOperand(MCOperand::createImm(NOP));
+    Instr.addOperand(MCOperand::createInst(SubInstr));
+  }
+
+  for (uint32_t i = 0; i < 2; ++i) {
+    if (HasCS[i]) {
+      MCInst *SubInstr = getContext().createMCInst();
+      Result = decodeInstruction(DecoderTableE2K_CS[i], *SubInstr, CS[i], Address, this, STI);
+      if (Result == MCDisassembler::Fail)
+        return Result;
+      Instr.addOperand(MCOperand::createInst(SubInstr));
+    }
+  }
+
+  for (uint32_t i = 0; i < 6; ++i) {
+    if (HasALS[i]) {
+      MCInst *SubInstr = getContext().createMCInst();
+      if (HasALES[i]) {
+        uint64_t Inst48 = als_ales[i].ALS | (uint64_t(als_ales[i].ALES) << 32);
+        Result = decodeInstruction(DecoderTableE2K_ALES[i], *SubInstr, Inst48, Address, this, STI);
+      } else {
+        Result = decodeInstruction(DecoderTableE2K_ALS[i], *SubInstr, als_ales[i].ALS, Address, this, STI);
+      }
+      if (Result == MCDisassembler::Fail)
+        return Result;
+      Instr.addOperand(MCOperand::createInst(SubInstr));
+      unsigned Flags = i; // channel
+      if ((als_ales[i].ALS & 0x80000000UL) == 0x80000000UL) // speculative mode
+        Flags |= 0b1000;
+      Instr.setFlags(Flags);
+    }
+  }
+
+  return Result;
 }
 
 typedef DecodeStatus (*DecodeFunc)(MCInst &MI, unsigned insn, uint64_t Address,
